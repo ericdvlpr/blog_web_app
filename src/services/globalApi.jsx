5697e641dc50c1abbe02b7f0a55cda8d5412a50d
Eric Jaucian <eric.jaucian@gmail.com>
2025-04-20 09:34:32 +0800

    return api source

--- src/services/globalApi.jsx
+++ src/services/globalApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://dev.to/api/articles";
-const BASE_URL = "https://api.spaceflightnewsapi.net/v4/articles/";
+const BASE_URL = "https://dev.to/api/articles";
+// const BASE_URL = "https://api.spaceflightnewsapi.net/v4/articles/";
 
 const getPost = axios.get(BASE_URL);
 const getPostById = (id) => axios.get(BASE_URL + "/" + id);
